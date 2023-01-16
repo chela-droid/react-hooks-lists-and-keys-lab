efeeabcbd6aa161ad8d07a9c20e4f097f777bdb5
chela-droid <[200~chelangatcaroline70@gmail.com~>
2023-01-16 13:55:21 +0300

    completed assignments

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -7,6 +7,35 @@ function ProjectList({ projects }) {
     <div id="projects">
       <h2>My Projects</h2>
       <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {/* <div id="project-list">{projectElements}</div> */}
+        {/* myprojects = (projects.name) */}
+        <div>
+          {projects.map(project =>{
+            return (
+              // <div className="d-flex align-items-center">
+              //   <p className="font-weight-bold mx-5">{project.about}</p>
+              //   <p>{project.name}</p>
+              //   <p>{project.technologies}</p>
+
+              // </div>
+              
+                <table>
+                  <div>
+                <h2>{project.about}</h2>
+                <p>{project.name}</p>
+                <span>{project.technologies}</span>
+
+              </div>
+              </table>
+
+            )
+          })}
+          {/* }) */}
+        </div>
+
+
+      </div>
     </div>
   );
 }
